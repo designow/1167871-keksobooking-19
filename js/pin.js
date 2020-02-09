08c323f1de03e67406242f055b52b973cbf7395a
designow <alex.designow@yandex.ru>
2020-02-09 13:20:00 +0700

    C:/Program Files/Git/

--- js/pin.js
+++ js/pin.js
@@ -8,7 +8,7 @@
       var pinForAdd = pinTemplate.cloneNode(true);
       var pinImg = pinForAdd.querySelector('img');
       var pinButton = pinForAdd.querySelector('.map__pin');
-      pinButton.style = 'left: ' + (window.data.mocks[i].offer.location.x + window.map.MAP_PIN_SIZE/2) + 'px; top: ' + (window.data.mocks[i].offer.location.y + window.map.MAP_PIN_HEIGHT) + 'px';
+      pinButton.style = 'left: ' + (window.data.mocks[i].offer.location.x + window.map.MAP_PIN_SIZE / 2) + 'px; top: ' + (window.data.mocks[i].offer.location.y + window.map.MAP_PIN_HEIGHT) + 'px';
       pinImg.src = window.data.mocks[i].author.avatar;
       pinImg.alt = window.data.mocks[i].offer.title;
       fragment.appendChild(pinForAdd);
