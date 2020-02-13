cff0631c0f32398b5cbf8f1afa41ad3a2becc60a
Alexey Gerasimenko <46708513+designow@users.noreply.github.com>
2020-02-13 13:57:36 +0700

    Add .map__pin--active

--- js/pin.js
+++ js/pin.js
@@ -10,6 +10,13 @@
     // Показываем карточку через замыкания функции
     pinButton.addEventListener('click', function () {
       window.card.cardShow(num);
+      var pinButtons = document.querySelectorAll('.map__pin');
+      for (var i = 0; i < pinButtons.length; i++) {
+        if (pinButtons[i].classList.contains('map__pin--active')) {
+          pinButtons[i].classList.remove('map__pin--active');
+        }
+        pinButton.classList.add('map__pin--active');
+      }
     });
     pinButton.style = 'left: ' + (data.location.x + window.map.MAP_PIN_SIZE / 2) + 'px; top: ' + (data.location.y + window.map.MAP_PIN_HEIGHT) + 'px';
     pinImg.src = data.author.avatar;
